320a125d8144118f0c3e23ef0750ede05a4cdf82
Admin <mirgiyozom@gmail.com>
2022-04-08 21:48:14 +0500

    Initial commit

---
+++ app/src/main/java/com/example/fragment/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button1, button2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button1 = findViewById(R.id.btn1);
+        button2 = findViewById(R.id.btn2);
+
+        button1.setOnClickListener(view->{
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .replace(R.id.fragment, FirstFragment.class, null)
+                    .commit();
+        });
+        button2.setOnClickListener(view->{
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .replace(R.id.fragment, SecondFragment.class, null)
+                    .commit();
+        });
+    }
+}
