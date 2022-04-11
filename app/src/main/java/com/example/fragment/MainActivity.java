9c265d969b5c2c0da0841f8e0111342f0cb2e0cd
Admin <mirgiyozom@gmail.com>
2022-04-11 06:30:50 +0500

    fragment

--- app/src/main/java/com/example/fragment/MainActivity.java
+++ app/src/main/java/com/example/fragment/MainActivity.java
@@ -7,6 +7,8 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button button1, button2;
+    FirstFragment firstFragment;
+    SecondFragment secondFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,10 +17,12 @@ public class MainActivity extends AppCompatActivity {
         button2 = findViewById(R.id.btn2);
 
         button1.setOnClickListener(view->{
+
             getSupportFragmentManager().beginTransaction()
                     .setReorderingAllowed(true)
                     .replace(R.id.fragment, FirstFragment.class, null)
                     .commit();
+
         });
         button2.setOnClickListener(view->{
             getSupportFragmentManager().beginTransaction()
@@ -26,5 +30,8 @@ public class MainActivity extends AppCompatActivity {
                     .replace(R.id.fragment, SecondFragment.class, null)
                     .commit();
         });
+
+
     }
+
 }
--- app/src/main/java/com/example/fragment/SecondFragment.java
+++ app/src/main/java/com/example/fragment/SecondFragment.java
@@ -1,9 +1,12 @@
 package com.example.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,10 +14,35 @@ import androidx.fragment.app.Fragment;
 
 
 public class SecondFragment extends Fragment {
+    TextView tv;
+    Frist mCallback;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_second, container, false);
+        tv = view.findViewById(R.id.textview_second);
+
         return view;
     }
+    public void updateText(String text){
+        tv.setText(text);
+
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+            mCallback = (Frist) context;
+    }
+    @Override
+    public void onDetach() {
+        mCallback = null;
+        super.onDetach();
+    }
+
+    public interface Frist{
+        public void sendText(String text);
+    }
+
 }
