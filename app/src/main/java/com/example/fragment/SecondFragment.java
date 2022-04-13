b6c7fa1ab9ab27e2ab0737cb012466dc3536a162
Admin <mirgiyozom@gmail.com>
2022-04-13 21:25:57 +0500

    fragment work

--- app/src/main/java/com/example/fragment/SecondFragment.java
+++ app/src/main/java/com/example/fragment/SecondFragment.java
@@ -14,35 +14,12 @@ import androidx.fragment.app.Fragment;
 
 
 public class SecondFragment extends Fragment {
-    TextView tv;
-    Frist mCallback;
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_second, container, false);
-        tv = view.findViewById(R.id.textview_second);
-
         return view;
     }
-    public void updateText(String text){
-        tv.setText(text);
 
-    }
-
-    @Override
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
-            mCallback = (Frist) context;
-    }
-    @Override
-    public void onDetach() {
-        mCallback = null;
-        super.onDetach();
-    }
-
-    public interface Frist{
-        public void sendText(String text);
-    }
 
 }
